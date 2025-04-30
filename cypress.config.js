ab9db7fde35a39a168c70c3e7fa9718920ed8ff2
prashant teotia <prashantteotia73@gmail.com>
2025-04-30 13:39:10 +0530

    config chaneges

--- cypress.config.js
+++ cypress.config.js
@@ -5,8 +5,8 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    screenshotsFolder: "cypress/screenshots/",
-    videosFolder: "cypress/videos/",
+    screenshotsFolder: "cypress/screenshots",
+    videosFolder: "cypress/videos",
     video: true,
     screenshotOnRunFailure: true,
   },
